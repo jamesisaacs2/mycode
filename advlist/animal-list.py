cae33e044c4c0165984b9b7a1d033262aeaa3535
jamesisaacs2 <jkifao@gmail.com>
2022-11-08 23:29:10 +0000

    complex lists practice

---
+++ advlist/animal-list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Animal List"""
+
+def main():
+
+    # create a list called list1
+    list = ["fox", "fly", "ant", "bee", "cat", "dog", "cow", "hen"]
+
+    # display list1
+    print(list[0], list[1], list[2], list[3], list[4], list[5], list[6], list[7])
+
+main()
+
