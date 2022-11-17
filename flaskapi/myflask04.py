d111a9976893b1a78f8422f384ff9ece0d9b9ac1
jamesisaacs2 <jkifao@gmail.com>
2022-11-17 21:17:49 +0000

    Jinja templating for HTML and configs

--- flaskapi/myflask04.py
+++ flaskapi/myflask04.py
@@ -10,15 +10,18 @@ from flask import request
 from flask import render_template
 
 app = Flask(__name__)
+
 ## This is where we want to redirect users to
 @app.route("/success/<name>")
 def success(name):
     return f"Welcome {name}\n"
+
 # This is a landing point for users (a start)
 @app.route("/") # user can land at "/"
 @app.route("/start") # or user can land at "/start"
 def start():
     return render_template("postmaker.html") # look for templates/postmaker.html
+
 # This is where postmaker.html POSTs data to
 # A user could also browser (GET) to this location
 @app.route("/login", methods = ["POST", "GET"])
@@ -29,6 +32,7 @@ def login():
             user = request.form.get("nm") # grab the value of nm from the POST
         else: # if a user sent a post without nm then assign value defaultuser
             user = "defaultuser"
+
     # GET would likely come from a user interacting with a browser
     elif request.method == "GET":
         if request.args.get("nm"): # if nm was assigned as a parameter=value
@@ -36,6 +40,7 @@ def login():
         else: # if nm was not passed...
             user = "defaultuser" # ...then user is just defaultuser
     return redirect(url_for("success", name = user)) # pass back to /success with val for name
+
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=2224) # runs the application
 
